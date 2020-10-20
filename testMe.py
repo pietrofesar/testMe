320c7204992366e1279c8530e7848f8754796802
pietrofesar <pietrofesar@users.noreply.github.com>
2020-10-20 18:51:16 -0400

    Update testMe.py

--- testMe.py
+++ testMe.py
@@ -106,7 +106,7 @@ problemSets = {# chapter 1 Y. Liang
 def main():
     # validate arguments
     if len(sys.argv) == 1:
-        print(f'{A}name: testMe\nVersion: 3.0.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}')
+        print(f'{A}\nName: testMe\nVersion: 3.0.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
     elif len(sys.argv) == 2:
         try:
             # find and store the file
