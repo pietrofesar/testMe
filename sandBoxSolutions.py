27900812485861b58d33590a60c32d551607d747
pietrofesar <pietrofesar@users.noreply.github.com>
2020-11-12 12:00:37 -0500

    Update sandBoxSolutions.py

--- sandBoxSolutions.py
+++ sandBoxSolutions.py
@@ -32,7 +32,7 @@ def sandbox(file):
     n = random.randint(0, 10)
     m = random.randint(0, 10)
     child.sendline(f'import {studentModule} as m')
-    child.sendline(f'print(m.testFunction(n + m))')
+    child.sendline(f'print(m.testFunction(n, m))')
     answerKey = f'{n + m}'
     try:
         child.expect_exact(answerKey)
