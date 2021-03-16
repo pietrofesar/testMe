160110d2b26887ea694731b3ae050e2a97fbe74f
pietrofesar <pietrofesar@users.noreply.github.com>
2021-03-16 11:08:40 -0400

    Update testMe.py

--- testMe.py
+++ testMe.py
@@ -114,7 +114,7 @@ problemSets = {'sandbox.py' : sandbox,
 def main():
     # validate arguments
     if len(sys.argv) == 1:
-        print(f'{A}\nName: testMe\nVersion: 3.1.3.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
+        print(f'{A}\nName: testMe\nVersion: 3.1.4.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
     elif len(sys.argv) == 2:
         try:
             # find and store the file
