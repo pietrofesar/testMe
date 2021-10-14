da5cc6a612880c2ca323c42941e0c0bf386e694f
pietrofesar <pietrofesar@users.noreply.github.com>
2021-10-14 12:48:42 -0400

    Update testMe.py

--- testMe.py
+++ testMe.py
@@ -65,7 +65,7 @@ X = '\033[0m'       # reset
 def main():
     # validate arguments
     if len(sys.argv) == 1:
-        print(f'{A}\nName: testMe\nVersion: 3.2.2.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
+        print(f'{A}\nName: testMe\nVersion: 3.2.2.1\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
     elif len(sys.argv) == 2:
         try:
             # find and store the file
