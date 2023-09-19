c8f88c7d7b9e4f244a1e3ed54bb5ac8755f33b19
pietrofesar <pietrofesar@users.noreply.github.com>
2023-09-19 18:06:07 -0400

    Update updateTestMe.sh

--- updateTestMe.sh
+++ updateTestMe.sh
@@ -24,26 +24,32 @@ A='\033[0;36m'    # aqua
 BA='\033[1;36m'   # bold aqua
 X='\033[0m'       # reset
 echo -e "${Y}Collecting and overwriting testMe Python autograder utility.${X}"
-rm -r testMe
-mkdir testMe
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/sandBoxSolutions.py -O testMe/sandBoxSolutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch10Solutions.py -O testMe/ch10Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch1Solutions.py -O testMe/ch1Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch2Solutions.py -O testMe/ch2Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch3Solutions.py -O testMe/ch3Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch4Solutions.py -O testMe/ch4Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch5Solutions.py -O testMe/ch5Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch6Solutions.py -O testMe/ch6Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch7Solutions.py -O testMe/ch7Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch8Solutions.py -O testMe/ch8Solutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/helpers.py -O testMe/helpers.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/learningPythonSolutions.py -O testMe/learningPythonSolutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/siennaSolutions.py -O testMe/siennaSolutions.py
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/testMe.py -O testMe/testMe.py
+rm -r /home/testMeFolder
+mkdir /home/testMeFolder
 
-rm updateTestMe.sh 
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/updateTestMe.sh -O updateTestMe.sh
-rm testMe.sh 
-wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/testMe.sh -O testMe.sh
+# download source files
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/sandBoxSolutions.py -O /home/testMeFolder/sandBoxSolutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch10Solutions.py -O /home/testMeFolder/ch10Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch1Solutions.py -O /home/testMeFolder/ch1Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch2Solutions.py -O /home/testMeFolder/ch2Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch3Solutions.py -O /home/testMeFolder/ch3Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch4Solutions.py -O /home/testMeFolder/ch4Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch5Solutions.py -O /home/testMeFolder/ch5Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch6Solutions.py -O /home/testMeFolder/ch6Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch7Solutions.py -O /home/testMeFolder/ch7Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/ch8Solutions.py -O /home/testMeFolder/ch8Solutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/helpers.py -O /home/testMeFolder/helpers.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/learningPythonSolutions.py -O /home/testMeFolder/learningPythonSolutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/siennaSolutions.py -O /home/testMeFolder/siennaSolutions.py
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/testMe.py -O /home/testMeFolder/testMe.py
+
+
+#echo -e "${Y}Installing the updateTestMe utility${X}"
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/updateTestMe.sh -O /home/testMeFolder/updateTestMe.sh; sudo chmod +x /home/testMeFolder/updateTestMe.sh
+sudo mv /home/testMeFolder/updateTestMe.sh /home/testMeFolder/updateTestMe
+
+#echo -e "${Y}Installing the testMe utility${X}"
+sudo wget -q  wget https://raw.githubusercontent.com/pietrofesar/testMe/main/testMe -O /home/testMeFolder/testMe.sh; sudo chmod +x /home/testMeFolder/testMe.sh
+sudo mv /home/testMeFolder/testMe.sh /home/testMeFolder/testMe
 
 bash testMe.sh
