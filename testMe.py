efb3b6c62e5b171e312f9ca0f2517883ff8e1974
pietrofesar <pietrofesar@users.noreply.github.com>
2023-10-12 19:23:07 -0400

    Update testMe.py

--- testMe.py
+++ testMe.py
@@ -66,7 +66,7 @@ X = '\033[0m'  # reset
 def main():
     # validate arguments
     if len(sys.argv) == 1:
-      print(f'{A}\nName: testMe\nVersion: 4.0.1\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com{X}\n')
+      print(f'{A}\nName: testMe\nVersion: 4.1.0\nSummary: testMe is a homegrown autograder\nAuthor: Rocco Pietrofesa\nAuthor-email: pietrofesar@gmail.com\Armand Caringi helped too!{X}\n')
     # wrong number of arguments - not working
     elif len(sys.argv) != 2:
         print(f'{R}Wrong amount of arguments entered{X}')
